45e312b56ac370c958306ab2bf2ff0490beea548
GGURIMON <ehdrhks105@naver.com>
2024-09-06 16:40:33 +0900

    add: U, D in CRUD

--- backend/test/test_app.py
+++ backend/test/test_app.py
@@ -27,3 +27,29 @@ def test_create_diary_missing_content(client):
     response = client.post('/diaries', json={'title': 'Test Title'})
     assert response.status_code == 400
     assert response.json == {'error': 'Title and Content are required'}
+
+@pytest.fixture
+def create_sample_diary():
+    response = client.post('/diaries', json={'title': 'Test Title', 'content': 'Test Content'})
+    return response.json['id']
+
+def test_get_diary(client):
+    response = client.put(f'/diaries/1', json={'title': 'Updated Title', 'content': 'Updated Content'})
+    assert response.status_code == 200
+    assert response.json['title'] == 'Updated Title'
+    assert response.json['content'] == 'Updated Content'
+
+# url path로 넘겨받은 id가 db에 존재하지 않을 때
+def test_update_diary_not_found(client):
+    response = client.put('/diaries/99999', json={'title': 'Updated Title', 'content': 'Updated Content'})
+    assert response.status_code == 404
+    assert response.json['error'] == 'Diary Not Found'
+
+def test_다이어리_삭제_성공시(client):
+    response = client.delete(f'/diaries/1')
+    assert response.status_code == 200
+
+def test_없는_다이어리_삭제_시도(client):
+    response = client.delete(f'/diaries/99999')
+    assert response.status_code == 404
+    assert response.json['error'] == 'Diary Not Found'
